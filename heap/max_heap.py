c2dd2a77f45b7ab48af8e0045802abb87562027d
mohit rawat <mohitrawatssj4@gmail.com>
2024-11-26 15:21:50 +0530

    check max heap function

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,7 +3,7 @@ class heap:
         self.hp = []
 
     def parent(self, index):
-        return (index-1) //2
+        return (index - 1)//2
 
     def left(self, index):
         return 2 * index + 1
@@ -12,37 +12,39 @@ class heap:
         return 2 * index + 2
 
     def heapify_up(self, index):
-        while index > 0 and (self.hp[self.parent(index)] > self.hp[index]):
-            self.hp[self.parent(index)], self.hp[index] = self.hp[index],  self.hp[self.parent(index)]
+        while index > 0 and (self.hp[self.parent(index)] < self.hp[index]):
+            self.hp[self.parent(index)],  self.hp[index] = self.hp[index], self.hp[self.parent(index)]
             index = self.parent(index)
 
     def heapify_down(self, index):
         length = len(self.hp)
-        small = index
+        largest = index
         left = self.left(index)
         right = self.right(index)
 
-        if left < length and (self.hp[left] < self.hp[small]):
-            small = left
+        if left < length and (self.hp[index] > self.hp[left]):
+            largest = left
 
-        if right < length and (self.hp[right] < self.hp[small]):
-            small = right
+        if right < length and (self.hp[index] > self.hp[right]):
+            largest = right
 
-        if small != index:
-            self.hp[small], self.hp[index] = self.hp[index], self.hp[small]
-            self.heapify_down(small)
+        if largest != index:
+            self.hp[largest], self.hp[index] = self.hp[index], self.hp[largest]
+            self.heapify_down(largest)
+
 
 
     def insert(self, val):
+        print(self.hp)
         self.hp.append(val)
-        self.heapify_up(len(self.hp)-1)
+        self.heapify_up(len(self.hp) - 1)
 
     def pop(self):
         if len(self.hp) == 0:
             return None
-
         root = self.hp[0]
         last_element = self.hp.pop()
+
         if self.hp:
             self.hp[0] = last_element
             self.heapify_down(0)
@@ -51,8 +53,6 @@ class heap:
 
     def _print(self):
         print(self.hp)
-
-
 
 arr = heap()
 arr1 = heap()
@@ -63,8 +63,10 @@ for i in [10,5,20,3]:
 arr1.pop()
 arr1._print()
 
-for i in [10, 15, 20, 17, 25]:
+for i in [10,5,20,3]:
     arr.insert(i)
 
+
+arr._print()
 arr.pop()
 arr._print()
