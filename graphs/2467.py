7696a6cad114ca2f831e6d137b054b6d5c87a7ba
arch <mohitrawatssj4@gmail.com>
2024-12-29 19:12:16 +0530

    new problem most profitable

--- graphs/2467.py
+++ graphs/2467.py
@@ -1,5 +1,4 @@
 def mostProfitable(edges, bob, amount):
-    print(len(edges))
     adj_list = {}
     profit = []
     for k,v in edges:
@@ -9,32 +8,47 @@ def mostProfitable(edges, bob, amount):
             adj_list[v] = []
         adj_list[k].append(v)
         adj_list[v].append(k)
-    start = 0
-    stack = [(start,None, [])]
-    visited = [False] * (len(edges) + 1)
-    while stack:
-        node, parent, path = stack.pop()
-        if visited[node]:
-            continue
-        visited[node] = True
-        curr_path=path + [node]
-        if len(adj_list[node]) == 1 and adj_list[node][0] == parent:
-            print(curr_path)
-            if bob in curr_path:
-                s = bob
-                su = 0
-                for n in curr_path:
-                    su = n
-                    if n == s:
-                        su+=amount[n]/2
-                    elif n>s:
-                        su
-                    else:
+
+    def dfs(start):
+        stack = [(start,None, [])]
+        visited = [False] * (len(edges) + 1)
+        output = []
+        while stack:
+            node, parent, path = stack.pop()
+            if visited[node]:
+                continue
+            visited[node] = True
+            curr_path=path + [node]
+            if len(adj_list[node]) == 1 and adj_list[node][0] == parent:
+                if 0 in curr_path:
+                    output.append(curr_path)
+            for i in adj_list[node]:
+                if not visited[i] and parent != i:
+                    stack.append((i,node,curr_path))
+        return output
+
+    paths = dfs(0)
+    bobs_path = dfs(bob)
+    print(paths)
+    print(bobs_path)
+    profit = []
+    for p in paths:
+        for b in paths:
+            total = 0
+            i = 0
+            while i < len(p):
+                if p[i] == b[i]:
+                    total+= amount[p]/2
 
 
-        for i in adj_list[node]:
-            if not visited[i] and parent != i:
-                stack.append((i,node,curr_path))
+
+
+    #bob will decrese ..
+    #those decresed will not add value
+    #if intercepted half it 
+    #if crossed add value
+
+
 
 mostProfitable([[0,1],[1,2],[1,3],[3,4]],3,[-2,4,2,-4,6])
 
