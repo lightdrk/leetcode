465b5f85ec9e79b6f18342f8d28d5fcb49832c74
arch <mohitrawatssj4@gmail.com>
2025-02-03 09:18:28 +0530

    medium 22 done

--- medium/22.py
+++ medium/22.py
@@ -1,23 +1,47 @@
+#V1
 def generate(n):
-    par = ['(']*n + [')']*n
+    par = ['(',')']
     output = []
-    def bk(s,start,st):
-        if len(s) == n*2 and not st:
-            print(s)
-            output.append(s)
-            return
-        for i in range(start,n*2):
-            char = par[i]
-            if st and char == ')' and st[-1] == '(':
-                st.pop()
-                bk(s+char,start+1,st)
-                st.append('(')
+
+    def isValid(s):
+        st = []
+        for char in s:
+            if char == ')':
+                if st and st[-1] == '(':
+                    st.pop()
+                else:
+                    return False
             else:
                 st.append(char)
-                bk(s+char,start+1,st)
-                st.pop()
+        return not st
 
-    bk('',0,[])
+    def bk(s):
+        if len(s) == n*2:
+            if isValid(s):
+                output.append(s)
+            return
+        for i in range(2):
+            bk(s+par[i])
+
+    bk('')
     return output
-
 print(generate(3))
+
+
+
+
+#V2
+def generateV2(n):
+    output = []
+    def bk(s,oc,cc):
+        if len(s) == n*2:
+            output.append(s)
+            return
+        if oc < n:
+            bk(s+'(',oc+1,cc)
+        if cc < oc:
+            bk(s+')',oc,cc+1)
+
+    bk('',0,0)
+    return output
+print(generateV2(3))
