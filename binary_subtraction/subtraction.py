f5a2d2c47e49c2a161f82b7f8e6fb0437f0f6abe
arch <mohitrawatssj4@gmail.com>
2025-03-12 12:00:25 +0530

    binary operation

--- binary_subtraction/subtraction.py
+++ binary_subtraction/subtraction.py
@@ -5,9 +5,31 @@ to_ = int(input('subtract By: '))
 
 carry = 0
 bin_f = f"{from_:b}"
+bin_f = [char for char in bin_f]
+print(bin_f)
 bin_t = f"{to_:b}"
-idx = 0
+length = len(bin_f)
+lt = len(bin_t) - 1
+print(bin_t, lt)
 output = ''
-carry = '0'
-for char in bin_f[::-1]:
-    if cha
+borrow = 0
+for i in range(length-1,-1,-1):
+    if lt == -1:
+        break
+
+    if bin_f[i] == '0' and bin_t[lt] == '1':
+        while bin_f[i] != 1:
+            i-=1
+            borrow+=1
+        bin_f[i] = 2
+        while borrow > 0:
+            bin_f[i+1] = '1'
+            borrow-=1
+        output = '1' + output
+    elif bin_f[i] == bin_t[lt]:
+        output = '0' + output
+    elif bin_f[i] == '1' and bin_t[lt] == '0':
+        output = '1' + output
+    lt-=1
+
+print(output)
--- graphs/743.py
+++ graphs/743.py
@@ -1,25 +1,40 @@
-n = 4
-matrix = [[0]*(n+1) for _ in range(n+1)]
-times = [[2,1,1],[2,3,1],[3,4,1]]
+from collections import deque
 
-for i,j,v in times:
-    matrix[i][j] = v
+def min_time_taken(times: list[list[int]], n, k):
+    matrix = [[0]*(n+1) for _ in range(n+1)]
+    for i,j,v in times:
+        matrix[i][j] = v
+    print(matrix)
+    visited = set()
+    min_t = 99999999999
+    que = deque([])
+    que.append((k, 0))
+    while que:
+        print(que)
+        source, taken = que.popleft()
+        if not source in visited:
+            min_t = taken
+            visited.add(source)
+        for i, val in enumerate(matrix[source]):
+            if val and not i in visited:
+                que.append((i,taken+val))
+                matrix[source][i] = 0
+    return -1 if len(visited) != n else min_t
 
-print(matrix)
+testCases =testCases = [
+    ([[2,1,1], [2,3,1], [3,4,1]], 4, 2),  # Test case 1
+    ([[1,2,1]], 2, 2),  # Test case 2
+    ([[1,2,1], [2,3,2], [1,3,4]], 3, 1),  # Test case 3
+    ([[1,2,2], [2,3,3], [3,4,4], [4,5,5]], 5, 1),  # Test case 4
+    ([[1,2,5], [1,3,3], [3,4,2], [2,4,1]], 4, 1),  # Test case 5
+    ([[1,2,1], [1,3,1], [2,3,1]], 3, 1),  # Test case 6
+    ([[1,2,2], [1,3,1], [2,3,1], [3,4,2]], 4, 1),  # Test case 7
+    ([[1,2,1], [1,3,2], [3,2,2]], 3, 1),  # Test case 8
+    ([[1,2,10], [2,3,1], [3,4,1], [4,5,1]], 5, 1),  # Test case 9
+    ([[1,2,3], [2,3,1], [1,3,5]], 3, 1),  # Test case 10
+]
 
-taken = 0
 
-visited = set()
-stack = [2]
-while stack:
-    node = stack.pop()
-    if not node in visited:
-        visited.add(node)
-    for i,v in enumerate(matrix[node]):
-        if v:
-            stack.append(i)
+for test, n, k in testCases:
+    print(min_time_taken(test,n,k))
 
-print(taken)
-min_ = 0
-def dfs(taken):
-    if
