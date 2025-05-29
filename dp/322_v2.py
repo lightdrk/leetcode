2df3b008d7a85764c5b4dc971bff040d3374882e
lightdrk <mohitrawatssj4@gmail.com>
2025-05-29 11:50:59 +0530

    update

--- dp/322_v2.py
+++ dp/322_v2.py
@@ -1,16 +1,22 @@
 def coin(coins, amount):
     length = len(coins)
-    ans = [0]
-    def recur(i,a):
+    def recur(a):
         if a == 0:
-            return True
-        if i >= length:
-            return False
-        if a >= coins[i]:
-            what = (recur(i+1, a-coins[i]) or recur(i+1, a))
-        return ans
+            return 0
+        if a < 0:
+            return float('inf')
+        min_m = float('inf')
+        for i in range(length):
+            ans = recur(a - coins[i])
+            if ans != float('inf'):
+                min_m = min(ans +1, min_m)
 
-    return recur(0,amount)
+        return min_m
+    ans = recur(amount)
+    return  ans if ans != float('inf') else -1
 
 print(coin([1,2,5], 11))
 print(coin([2], 3))
+
+def coin(coins, amount):
+
