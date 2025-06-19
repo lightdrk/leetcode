2034658ee99381e8c288552426b78c6a800b2de7
lightdrk <mohitrawatssj4@gmail.com>
2025-06-19 12:33:18 +0530

    update

--- dp/10.py
+++ dp/10.py
@@ -18,3 +18,36 @@ that mean last should be equal
 
 
 '''
+
+'''
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+'''
+
+''' 
+solution is kind of if i get '.' or both i == j then we can simple move by one dp(i+1,j+1)
+if i get '.*' then we can do two things 
+ -> move j+2 by assume or can be becaus their is nothing to make it equal to 
+## abc . a.*bc here we dont need .* 
+another thing needs to be done simuteniously is we can move i+1 by one assuming thier might be a difff
+'''
+
+
+def isMatch(s,p):
+    ls = len(s)
+    lp = len(p)
+    def dp(i=0,j=0):
+        if j == lp:
+            return i == ls
+        
+        first = i < ls and (p[j] == '.' or s[i] == p[j])
+        if j+1 < lp and p[j+1] == '*':
+            return dp(i,j+2) or (first and dp(i+1,j))
+
+        return first and dp(i+1,j+1)
+    return dp(0,0)
+
+print(isMatch("aab", "c*a*b"))
