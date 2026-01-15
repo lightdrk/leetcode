fba06287716e6c56a49184a1ab9e49cec3293861
lightdrk <mohitrawatssj4@gmail.com>
2026-01-15 14:36:54 +0530

    update

--- sort/insertion_sort_1.py
+++ sort/insertion_sort_1.py
@@ -11,6 +11,49 @@ def insertion_sort(arr):
         arr[j+1] = key
     return arr
 
+def insertion_sort_reverse(arr):
+    l = len(arr)
+    for i in range(l):
+        key = arr[i]
+        j = i - 1
+        while 0 <= j and arr[j] <= key:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+    return arr
+
+class Node:
+    def __init__(self,val,next=None):
+        self.val = val
+        self.next = next
+
+
+def l_to_ll(arr):
+    root = head = None
+    for n in arr:
+        if not root:
+            root = Node(n)
+            head = root
+        else:
+            head.next = Node(n)
+            head = head.next
+    return root
+
+def printL(root):
+    while root:
+        print(root.val, end='->')
+        root = root.next
+    print()
+    return 
+
+def insertion_sort_linked_list(head):
+    curr = head
+    while curr:
+        tmp = curr
+        tmp_next = curr.next
+
 
 for t in test_case:
     print(insertion_sort(t))
+    print(insertion_sort_reverse(t))
+    printL(l_to_ll(t))
