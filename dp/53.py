9ca89d80561038358ca3f1a6c372edd8dfdff801
lightdrk <mohitrawatssj4@gmail.com>
2026-06-10 17:35:34 +0530

    update

--- dp/53.py
+++ dp/53.py
@@ -13,6 +13,7 @@
 #print(mxSub([1]))
 #print(mxSub([5,4,-1,7,8]))
 
+'''
 def mxSub(nums):
     ans = float('-inf')
     length = len(nums)
@@ -20,9 +21,18 @@ def mxSub(nums):
         for j in range(i,length):
             ans = max(ans, sum(nums[i:j]))
 
-
     return ans
+'''
+from functools import cache
 
+def mxSub(nums):
+    l = len(nums)
+    @cache
+    def dp(idx):
+        if idx >= l:
+            return  float('-inf')
+        return max(nums[idx]+dp(idx+1),nums[idx])
+    return max(dp(i) for i in range(l))
 
 print(mxSub([-2,1,-3,4,-1,2,1,-5,4]))
 
